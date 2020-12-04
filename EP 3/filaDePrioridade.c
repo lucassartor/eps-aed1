b764c1126bd04ae92a4c436c334aa4f2d74c51dc
lucassartor <49413772+lucassartor@users.noreply.github.com>
2020-12-04 12:29:32 -0300

    ep3 concluido obg deus

--- EP 3/filaDePrioridade.c
+++ EP 3/filaDePrioridade.c
@@ -11,13 +11,15 @@
 
 #include "filaDePrioridade.h"
 
-PFILA criarFila(int max) {
+enum lado {esquerdo, direito};
+
+PFILA criarFila(int max){
     PFILA res = (PFILA) malloc(sizeof(FILADEPRIORIDADE));
     res->maxElementos = max;
-    res->arranjo = (PONT *) malloc(sizeof(PONT) * max);
-    res->heap = (PONT *) malloc(sizeof(PONT) * max);
+    res->arranjo = (PONT*) malloc(sizeof(PONT)*max);
+    res->heap = (PONT*) malloc(sizeof(PONT)*max);
     int i;
-    for (i = 0; i < max; i++) {
+    for (i=0;i<max;i++) {
         res->arranjo[i] = NULL;
         res->heap[i] = NULL;
     }
@@ -25,275 +27,198 @@ PFILA criarFila(int max) {
     return res;
 }
 
-void exibirLog(PFILA f) {
+
+
+void exibirLog(PFILA f){
     printf("Log [elementos: %i]\n", f->elementosNoHeap);
     PONT atual;
     int i;
-    for (i = 0; i < f->elementosNoHeap; i++) {
+    for (i=0;i<f->elementosNoHeap;i++){
         atual = f->heap[i];
         printf("[%i;%f;%i] ", atual->id, atual->prioridade, atual->posicao);
     }
     printf("\n\n");
 }
 
-int tamanho(PFILA f) {
-    int tam = 0;
+int tamanho(PFILA f){
+    int tam = 0, i = 0;
 
-    while (f->heap[tam] != NULL) {
+    while (f->heap[i] != NULL) {
         tam++;
+        i++;
     }
 
     return tam;
 }
 
-bool inserirElemento(PFILA f, int id, float prioridade) {
+// Usado apenas na inserção e no aumento de prioridade
+void maxHeapfyBottomUp(PFILA f, PONT elemento) {
+
+    // Nó Raiz não possui pai
+    if (elemento->posicao == 0) return;
+
+    int indicePai = (elemento->posicao - 1) / 2;
+
+    if (elemento->prioridade > f->heap[indicePai]->prioridade) {
+        PONT tmpNo = f->heap[indicePai];
+        int tmpPosicao = elemento->posicao;
+
+        // Coloca o ex-filho no lugar do pai
+        f->heap[indicePai] = elemento;
+        elemento->posicao = indicePai;
+
+        // Coloca o ex-pai no lugar do filho
+        f->heap[tmpPosicao] = tmpNo;
+        tmpNo->posicao = tmpPosicao;
+
+        maxHeapfyBottomUp(f, elemento);
+    }
+
+    // Fim da função
+}
+
+void maxHeapfyTopDown(PFILA f, PONT elemento) {
+    if (elemento == NULL) return;
+
+
+    int indiceFilhoEsq = (2 * elemento->posicao) + 1;
+    int indiceFilhoDir = (2 * elemento->posicao) + 2;
+
+    // Testa se o índice dos filhos é válido
+    if (indiceFilhoDir >= f->elementosNoHeap) indiceFilhoDir = -1;
+    if (indiceFilhoEsq >= f->elementosNoHeap) indiceFilhoEsq = -1;
+
+    if (indiceFilhoEsq == -1) return; // Elemento é uma folha, já que se ele não tem nem filho à esquerda, sequer terá à direita
+
+    // Assumimos por ora que o filho à esquerda é o maior
+    int indiceMaiorFilho = indiceFilhoEsq;
+
+    if (indiceFilhoDir >= 2) { // Testamos se existe um filho à direita. O menor índice possível para um nó seja filho à direita de outro nó é pelo menos 2
+
+        if (f->heap[indiceFilhoEsq]->prioridade < f->heap[indiceFilhoDir]->prioridade) {
+            indiceMaiorFilho = indiceFilhoDir;
+        }
+    }
+
+
+
+    if (elemento->prioridade < f->heap[indiceMaiorFilho]->prioridade) {
+        PONT tmpNo = f->heap[indiceMaiorFilho];
+        int tmpPosicao = elemento->posicao;
+
+        // Coloca o ex-pai no lugar do filho
+        f->heap[indiceMaiorFilho] = elemento;
+        elemento->posicao = indiceMaiorFilho;
+
+        // Coloca o ex-filho no lugar do pai
+        f->heap[tmpPosicao] = tmpNo;
+        tmpNo->posicao = tmpPosicao;
+
+        maxHeapfyTopDown(f, elemento);
+    }
+}
+
+bool inserirElemento(PFILA f, int id, float prioridade){
     bool res = false;
 
-    float *resposta = NULL;
+    // Condições de escape
 
-    //Verifica se o elemento eh valido, caso nao for retorna false
-    if (id < 0 || id >= f->maxElementos || consultarPrioridade(f, id, resposta))
-        return res;
+    if (id < 0 || id >= f->maxElementos) return res;
+    if (f->arranjo[id] != NULL) return res;
 
-    //Elemento eh valido, entao eh alocado espaco na memoria e colocado no arranjo
+    // Aloca memória pro novo elemento
     PONT novoElemento = (PONT) malloc(sizeof(ELEMENTO));
+
+    // Preenche os campos do novo elemento (a posição pode ser atualizada na função maxHeapfy)
     novoElemento->id = id;
     novoElemento->prioridade = prioridade;
+    novoElemento->posicao = f->elementosNoHeap;
+
+    // Atualiza informações da fila
     f->arranjo[id] = novoElemento;
+    f->heap[f->elementosNoHeap] = novoElemento;
+    (f->elementosNoHeap)++;
 
-    int i;
+    // Correção das posições
+    maxHeapfyBottomUp(f, novoElemento);
 
-    //Adiciona elemento no heap
-    for (i = 0; i < f->maxElementos; i++) {
-        //Caso o heap esteja vazio
-        if (f->heap[i] == NULL) {
-            novoElemento->posicao = 0;
 
-            f->heap[i] = novoElemento;
-            f->elementosNoHeap = f->elementosNoHeap + 1;
-            i = f->maxElementos;
-        }
-
-            //Caso o elemento seja o primeiro de uma fila nao vazia
-        else if (novoElemento->prioridade > f->heap[0]->prioridade) {
-            int j = f->maxElementos - 1;
-            while (j >= 0) {
-                if (f->heap[j] != NULL) {
-                    f->heap[j + 1] = f->heap[j];
-                    f->heap[j + 1]->posicao++;
-                }
-                j--;
-            }
-
-            novoElemento->posicao = 0;
-            f->heap[0] = novoElemento;
-
-            f->elementosNoHeap = f->elementosNoHeap + 1;
-            i = f->maxElementos;
-        }
-
-            //Caso o elemento pertenca ao final da fila
-        else if (f->heap[i + 1] == NULL) {
-            novoElemento->posicao = f->heap[i]->posicao + 1;
-
-            f->heap[i + 1] = novoElemento;
-            f->elementosNoHeap = f->elementosNoHeap + 1;
-            i = f->maxElementos;
-        }
-
-            //Caso o elemento pertenca ao meio da fila
-        else if (f->heap[i]->prioridade > novoElemento->prioridade &&
-                 f->heap[i + 1]->prioridade < novoElemento->prioridade) {
-            int j = f->maxElementos - 1;
-            while (j > i) {
-                if (f->heap[j] != NULL) {
-                    f->heap[j + 1] = f->heap[j];
-                    f->heap[j + 1]->posicao++;
-                }
-                j--;
-            }
-
-            novoElemento->posicao = f->heap[i]->posicao + 1;
-            f->heap[i + 1] = novoElemento;
-
-            f->elementosNoHeap = f->elementosNoHeap + 1;
-            i = f->maxElementos;
-        }
-    }
-
+    // Fim
     res = true;
+
     return res;
 }
 
-bool aumentarPrioridade(PFILA f, int id, float novaPrioridade) {
+bool aumentarPrioridade(PFILA f, int id, float novaPrioridade){
     bool res = false;
 
-    float *resposta = NULL;
+    // Condições de invalidez
+    if (id < 0 || id >= f->maxElementos) return res;
+    PONT elemento = f->arranjo[id];
+    if (elemento == NULL) return res;
+    if (elemento->prioridade >= novaPrioridade) return res;
 
-    //Verifica se o elemento eh valido, caso nao for retorna false
-    if (id < 0 || id >= f->maxElementos || !consultarPrioridade(f, id, resposta))
-        return res;
+    // Troca de prioridade
+    elemento->prioridade = novaPrioridade;
 
-    //Procura qual eh o elemento dentro do heap
-    int i;
-    int posicaoHeap;
-    for (i = 0; i < f->maxElementos; i++) {
-        if (f->heap[i]->id == id) {
-            //Verifica se elemento ja possui prioridade maior ou igual a nova prioridade, se sim retorna falso
-            if (f->heap[i]->prioridade >= novaPrioridade)
-                return res;
+    // Correção das posições
+    maxHeapfyBottomUp(f, elemento);
 
-            posicaoHeap = i;
-            i = f->maxElementos;
-        }
-    }
-
-    //Muda a prioridade
-    f->heap[posicaoHeap]->prioridade = novaPrioridade;
-
-    //Cria elemento aux
-    PONT pontElemento;
-    pontElemento = f->heap[posicaoHeap];
-
-    //Realoca as posicoes
-    i = posicaoHeap;
-    bool aux = false;
-
-    //Verifica se ja n eh o primeiro da fila
-    if (i > 0) {
-        while (!aux) {
-            //Caso nao seja o ultimo da fila
-            if (f->heap[i + 1]) {
-                if (f->heap[i - 1]->prioridade >= f->heap[i]->prioridade &&
-                    f->heap[i + 1]->prioridade <= f->heap[i]->prioridade)
-                    aux = true;
-            }
-
-            //Caso seja o ultimo da fila
-            else {
-                if (f->heap[i - 1]->prioridade > f->heap[i]->prioridade)
-                    aux = true;
-            }
-
-            if (!aux) {
-                f->heap[i] = f->heap[i - 1];
-                f->heap[i - 1] = pontElemento;
-
-                f->heap[i]->posicao++;
-                f->heap[i - 1]->posicao--;
-            }
-
-            i--;
-            if (i == 0)
-                aux = true;
-        }
-    }
-
+    // Fim
     res = true;
+
     return res;
 }
 
-bool reduzirPrioridade(PFILA f, int id, float novaPrioridade) {
+bool reduzirPrioridade(PFILA f, int id, float novaPrioridade){
     bool res = false;
+    // Condições de invalidez
+    if (id < 0 || id >= f->maxElementos) return res;
+    PONT elemento = f->arranjo[id];
+    if (elemento == NULL) return res;
+    if (elemento->prioridade <= novaPrioridade) return res;
 
-    float *resposta = NULL;
+    // Troca de prioridade
+    elemento->prioridade = novaPrioridade;
 
-    //Verifica se o elemento eh valido, caso nao for retorna false
-    if (id < 0 || id >= f->maxElementos || !consultarPrioridade(f, id, resposta)) //mudar essa bagaça
-        return res;
+    // Correção das posições
+    maxHeapfyTopDown(f, elemento);
 
-    //Procura qual eh o elemento dentro do heap
-    int i;
-    int posicaoHeap;
-    for (i = 0; i < f->maxElementos; i++) {
-        if (f->heap[i]->id == id) {
-            //Verifica se elemento ja possui prioridade menor ou igual a nova prioridade, se sim retorna falso
-            if (f->heap[i]->prioridade <= novaPrioridade)
-                return res;
-
-            posicaoHeap = i;
-            i = f->maxElementos;
-        }
-    }
-
-    //Muda a prioridade
-    f->heap[posicaoHeap]->prioridade = novaPrioridade;
-
-    //Cria elemento aux
-    PONT pontElemento;
-    pontElemento = f->heap[posicaoHeap];
-
-    //Realoca as posicoes
-    i = posicaoHeap;
-    bool aux = false;
-
-    //Verifica se ja n eh o ultimo da fila
-    if (i < f->maxElementos - 1) {
-        while (!aux) {
-            //Caso nao seja o primeiro da fila
-            if (f->heap[i]->posicao > 0) {
-                if (f->heap[i - 1]->prioridade >= f->heap[i]->prioridade &&
-                    f->heap[i + 1]->prioridade <= f->heap[i]->prioridade)
-                    aux = true;
-            }
-
-            //Caso seja o primeiro da fila
-            else {
-                if (f->heap[i + 1]->prioridade < f->heap[i]->prioridade)
-                    aux = true;
-            }
-
-            if (!aux) {
-                f->heap[i] = f->heap[i + 1];
-                f->heap[i + 1] = pontElemento;
-
-                f->heap[i]->posicao--;
-                f->heap[i + 1]->posicao++;
-            }
-
-            i++;
-            if (i == f->elementosNoHeap - 1)
-                aux = true;
-        }
-    }
-
+    // Fim
     res = true;
+
     return res;
 }
 
-PONT removerElemento(PFILA f) {
+PONT removerElemento(PFILA f){
     PONT res = NULL;
 
-    if (f->elementosNoHeap == 0)
-        return res;
+    if (f->elementosNoHeap == 0) return res;
 
-    //Sava o ponteiro do elemento removido e o remove do arranjo
-    PONT elementoRemovido = f->heap[0];
-    f->arranjo[elementoRemovido->id] = NULL;
+    // Armazena o elemento a ser removido
+    res = f->heap[0];
 
-    //Reorganiza o heap
+    // Coloca o último elemento no lugar do primeiro
+    f->heap[0] = f->heap[f->elementosNoHeap - 1];
+    f->heap[0]->posicao = 0;
 
-    if (f->elementosNoHeap == 1) {
-        f->heap[0] = NULL;
-    } else {
-        int j = 0;
-        while (j < f->elementosNoHeap - 1) {
 
-            f->heap[j] = f->heap[j + 1];
-            f->heap[j]->posicao--;
-            f->heap[j + 1] = NULL;
+    // Limpa a posição do último elemento e diminui a contagem de elementos
+    f->heap[f->elementosNoHeap - 1] = NULL;
+    (f->elementosNoHeap)--;
 
-            j++;
-        }
-    }
+    // Limpa o registro do elemento removido do array de id's
 
-    f->elementosNoHeap--;
+    f->arranjo[res->id] = NULL;
 
-    res = elementoRemovido;
+
+    maxHeapfyTopDown(f, f->heap[0]);
+
+
     return res;
 }
 
-bool consultarPrioridade(PFILA f, int id, float *resposta) {
+bool consultarPrioridade(PFILA f, int id, float* resposta){
     bool res = false;
 
     //Verifica se o elemento eh valido, caso nao for retorna false
@@ -301,28 +226,13 @@ bool consultarPrioridade(PFILA f, int id, float *resposta) {
         return res;
 
     //Verifica se existe um elemento na fila com esse id
-    int i = 0;
-    while (f->heap[i] != NULL) {
-        if (f->heap[i]->id == id) {
-            res = true;
-            resposta = &f->heap[i]->prioridade;
-            break;
-        }
-        i++;
-    }
+    PONT elemento = f->arranjo[id];
+    if (elemento == NULL)
+        return res;
+
+    *resposta = elemento->prioridade;
+
+    res = true;
 
     return res;
-}
-
-int main() {
-    PFILA f = criarFila(4);
-    inserirElemento(f,0,200);
-    inserirElemento(f,1,201);
-    inserirElemento(f,2,201.5);
-    inserirElemento(f,3,10.5);
-    exibirLog(f);
-    aumentarPrioridade(f,0,201.6);
-    exibirLog(f);
-
-    return 0;
 }
---
+++ EP 3/test.c
@@ -0,0 +1,563 @@
+/*************************** TESTES PARA O EP 3 - AED 1 ********************************/
+/** AUTORES: EGYDIO                                                                   **/
+/**          EMANUEL @EMMANUCA														  **/
+/**          RAUL @rmellos			  												  **/
+/**                                                                                   **/
+/** ATENCAO: OS TESTES REALIZADOS NESTE ARQUIVO PODEM NAO ABRANGER TODAS AS SITUACOES **/
+/** TESTADAS PELOS PROFESSORES                                                        **/
+/**                                                                                   **/
+/***************************************************************************************/
+ 
+#include "filaDePrioridade.c"
+
+int countArray(int arr[], int size) {
+    int res = 0;
+	 
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == 1) {
+			res++;
+			printf("Caso %i falhou\n", i);
+		}
+    }
+	 
+    return res;
+}
+
+bool position(PFILA f) {
+	for (int i = 0; i < f -> maxElementos; i++) {
+		if (i >= f -> elementosNoHeap) break;
+		if (f -> heap[i] -> posicao != i) return false;
+	}
+	return true;
+}
+ 
+bool isMaxHeap(PFILA f) {
+	if (!f) return true;
+	for (int i = 0; i < f -> maxElementos; i++) {
+		int esq = 2*i + 1;
+		int dir = 2*i + 2;
+		if (i >= f -> elementosNoHeap) break;
+		 
+		if (esq < f -> elementosNoHeap) {
+			if ( f -> heap[i] -> prioridade < f -> heap[esq] -> prioridade) return false;
+			 
+			if (dir < f -> elementosNoHeap && 
+				f -> heap[i] -> prioridade < f -> heap[dir] -> prioridade
+			   ) return false;
+		}
+	}
+	return true;
+}
+ 
+int main() {
+	PFILA f = criarFila(11);
+	float resposta;
+	exibirLog(f);
+
+
+	int teste0[16];
+	for (int i = 0; i < 16; i++) {
+        teste0[i] = 0;
+    }
+
+	printf("TESTE 0 - RESTRICOES\n\n");
+
+	// Remover no heap vazio
+	if(removerElemento(f)) {
+	    printf("1-incorreto\n");
+	    teste0[0] = 1;
+	} else printf("1-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Aumentar prioridade no heap vazio
+	if(aumentarPrioridade(f,0,1)) {
+	    printf("2-incorreto\n");
+	    teste0[1] = 1;
+	} else printf("2-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Inserir com id negativo
+	if(inserirElemento(f,-5,1)) {
+	    printf("3-incorreto\n");
+	    teste0[2] = 1;
+	} else printf("3-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Inserir com prioridade negativa
+	if(inserirElemento(f,5,-1)) printf("4-CORRETO\n");
+	else {
+	    printf("4-incorreto\n");
+	    teste0[3] = 1;
+	}
+	exibirLog(f);
+
+	// Inserir com ID maior que o max.
+	if(inserirElemento(f,11,1)) {
+	    printf("5-incorreto\n");
+	    teste0[4] = 1;
+	} else printf("5-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Inserir em condições normais
+	if(inserirElemento(f,0,1)) printf("6-CORRETO\n");
+	else {
+	    printf("6-incorreto\n");
+	    teste0[5] = 1;
+	}
+	exibirLog(f);
+	 
+	if(isMaxHeap(f)) printf("7-CORRETO\n");
+	else {
+	    printf("7-incorreto(propriedades do heap max quebradas)\n");
+        teste0[6] = 1;
+	}
+
+	// Inserir em id já existente
+	if(inserirElemento(f,0,5)) {
+	    printf("8-incorreto\n");
+	    teste0[7] = 1;
+	} else printf("8-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Consultar a prioridade do primeiro elemento
+	if(consultarPrioridade(f,0,&resposta)) {
+		if (resposta == 1.0) printf("9-CORRETO:%f\n",resposta); //ERRO
+		else {
+		    printf("9-incorreto(Retornou prioridade incorreta");
+		    teste0[8] = 1;
+		}
+	} else {
+	    printf("9-incorreto(Retornou falso)\n");
+	    teste0[8] = 1;
+	}
+	exibirLog(f);
+
+	// Reduzir prioridade no heap vazio
+	if(reduzirPrioridade(f,0,1)) {
+	    printf("10-incorreto\n");
+	    teste0[9] = 1;
+	} else printf("10-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Aumentar prioridade que jah eh maior que a nova
+	if(aumentarPrioridade(f,0,0)) {
+	    printf("11-incorreto\n");
+	    teste0[10] = 1;
+	} else printf("11-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Reduzir prioridade que jah eh menor que a nova
+	if(reduzirPrioridade(f,0,10)) {
+	    printf("12-incorreto\n");
+	    teste0[11] = 1;
+	} else printf("12-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Aumentando a prioridade de um ID inexistente no heap
+	if(aumentarPrioridade(f,3,1)) {
+	    printf("13-incorreto\n");
+	    teste0[12] = 1;
+	} else printf("13-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Reduzindo a prioridade de um ID inexistente no heap
+	if(reduzirPrioridade(f,4,10)) {
+	    printf("14-incorreto\n");
+	    teste0[13] = 1;
+	} else printf("14-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Consultar prioridade de elemento com id maior que o maximo permitido
+	if(consultarPrioridade(f,12,&resposta)) {
+	    printf("15-incorreto\n");
+	    teste0[14] = 1;
+	} else printf("15-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Consultar prioridade de elemento nao existente
+	if(consultarPrioridade(f,2,&resposta)) {
+	    printf("16-incorreto\n");
+	    teste0[15] = 1;
+	} else printf("16-ERRO (ESPERADO)\n");
+	exibirLog(f);
+// 
+//-----------------------------------------------------------------------------------------------//
+// 
+	printf("\nTESTE 1 - REMOCAO\n\n");
+
+	int teste1[25];
+	for (int i = 0; i < 24; i++) {
+        teste1[i] = 0;
+    }
+
+	PONT el;
+	// Removendo todos os elementos do heap
+	if( (el = removerElemento(f)) ) {
+		if(el->prioridade == 1.0) printf("1-CORRETO\n");
+		else {
+		    printf ("1-incorreto(Prioridade incorreta");
+		    teste1[0] = 1;
+		}
+	} else {
+	    printf("1-incorreto\n");
+        teste1[0] = 1;
+	}
+	exibirLog(f);
+	 
+	if( (el = removerElemento(f)) ) {
+		if(el->prioridade == -1.0) printf("2-CORRETO\n");
+		else {
+		    printf ("2-incorreto(Prioridade incorreta)\n");
+            teste1[1] = 1;
+		}
+	} else {
+	    printf("2-incorreto(retornou false)\n");
+        teste1[1] = 1;
+	}
+	exibirLog(f);
+
+	// Removendo de um heap vazio após remover elementos
+	if(removerElemento(f)) {
+	    printf("3-incorreto\n");
+        teste1[2] = 1;
+	} else printf("3-ERRO (ESPERADO)\n");
+	exibirLog(f);
+
+	// Inserindo elementos
+	if(inserirElemento(f,3,3)) printf("4-CORRETO\n");
+	else {
+	    printf("4-incorreto\n");
+        teste1[3] = 1;
+	}
+	exibirLog(f);
+
+	if(inserirElemento(f,1,1)) printf("5-CORRETO\n");
+	else {
+	    printf("5-incorreto\n");
+        teste1[4] = 1;
+	}
+	exibirLog(f);
+	 
+	if(isMaxHeap(f)) printf("6-CORRETO\n");
+	else {
+	    printf("6-incorreto(propriedades do heap max quebradas)\n");
+        teste1[5] = 1;
+	}
+
+	if(inserirElemento(f,2,2)) printf("7-CORRETO\n");
+	else {
+	    printf("7-incorreto\n");
+        teste1[6] = 1;
+	}
+	exibirLog(f);
+
+	if(isMaxHeap(f)) printf("8-CORRETO\n");
+	else {
+	    printf("8-incorreto(propriedades do heap max quebradas)\n");
+        teste1[7] = 1;
+	}
+	 
+	// Reduzindo a prioridade de um elemento 
+	if(reduzirPrioridade(f,3,0)) printf("9-CORRETO\n");
+	else {
+	    printf("9-incorreto\n");
+        teste1[8] = 1;
+	}
+	exibirLog(f);
+	
+	if(isMaxHeap(f)) printf("10-CORRETO\n");
+	else {
+	    printf("10-incorreto(propriedades do heap max quebradas)\n");
+        teste1[9] = 1;
+	}
+	 
+	// Inserindo elementos
+	if(inserirElemento(f,9,3)) printf("11-CORRETO\n");
+	else {
+	    printf("11-incorreto\n");
+        teste1[10] = 1;
+	}
+	exibirLog(f);
+	
+	if(isMaxHeap(f)) printf("12-CORRETO\n");
+	else {
+	    printf("12-incorreto(propriedades do heap max quebradas)\n");
+        teste1[11] = 1;
+	}
+	 
+	if(inserirElemento(f,10,2)) printf("13-CORRETO\n");
+	else {
+	    printf("13-incorreto\n");
+        teste1[12] = 1;
+	}
+	exibirLog(f);
+	 
+	if(isMaxHeap(f)) printf("14-CORRETO\n");
+	else {
+	    printf("14-incorreto(propriedades do heap max quebradas)\n");
+        teste1[13] = 1;
+	}
+
+	if(inserirElemento(f,6,3)) printf("15-CORRETO\n");
+	else {
+	    printf("15-incorreto\n");
+        teste1[14] = 1;
+	}
+	exibirLog(f);
+
+	if(isMaxHeap(f)) printf("16-CORRETO\n");
+	else {
+	    printf("16-incorreto(propriedades do heap max quebradas)\n");
+        teste1[15] = 1;
+	}
+	 
+	// Reduzindo prioridades
+	if(reduzirPrioridade(f,2,1)) {
+		el = f -> arranjo[2];
+		if (el->prioridade == 1.0) printf("17-CORRETO\n");
+		else {
+		    printf("17-incorreto(prioridade errada)\n");
+            teste1[16] = 1;
+		}
+	} else {
+	    printf("17-incorreto(retornou false)\n");
+        teste1[16] = 1;
+	}
+	exibirLog(f);
+	
+	if(isMaxHeap(f)) printf("18-CORRETO\n");
+	else {
+	    printf("18-incorreto(propriedades do heap max quebradas)\n");
+        teste1[17] = 1;
+	}
+	 
+	// Removendo Elementos
+	if( (el = removerElemento(f)) ) {
+		if (el -> id == 9 ) {
+			if (el -> prioridade == 3.0) printf("19-CORRETO\n");
+			else {
+			    printf("19-incorreto(prioridade errada)");
+                teste1[18] = 1;
+			}
+		} else {
+		    printf("19-incorreto(id errado)");
+            teste1[18] = 1;
+		}
+	} else {
+	    printf("19-incorreto(retornou false)\n");
+        teste1[18] = 1;
+	}
+	exibirLog(f);
+	 
+	if(isMaxHeap(f)) printf("20-CORRETO\n");
+	else {
+	    printf("20-incorreto(propriedades do heap max quebradas)\n");
+        teste1[19] = 1;
+	}
+	
+	if( (el = removerElemento(f)) ) {
+		if (el -> id == 6 ) {
+			if (el -> prioridade == 3.0) printf("21-CORRETO\n");
+			else {
+			    printf("21-incorreto(prioridade errada)");
+                teste1[20] = 1;
+			}
+		} else {
+		    printf("21-incorreto(id errado)");
+            teste1[20] = 1;
+		}
+	} else {
+	    printf("21-incorreto(retornou false)\n");
+        teste1[20] = 1;
+	}
+	exibirLog(f);
+	 
+	if(isMaxHeap(f)) printf("22-CORRETO\n");
+	else {
+	    printf("22-incorreto(propriedades do heap max quebradas)\n");
+        teste1[21] = 1;
+	}
+	 
+	if( (el = removerElemento(f)) ) {
+		if (el -> id == 10 ) {
+			if (el -> prioridade == 2.0) printf("23-CORRETO\n");
+			else {
+			    printf("23-incorreto(prioridade errada)");
+                teste1[22] = 1;
+			}
+		} else {
+		    printf("23-incorreto(id errado)");
+            teste1[22] = 1;
+		}
+	} else {
+	    printf("23-incorreto(retornou false)\n");
+        teste1[22] = 1;
+	}
+	exibirLog(f);
+	 
+	if(isMaxHeap(f)) printf("24-CORRETO\n");
+	else {
+	    printf("24-incorreto(propriedades do heap max quebradas)\n");
+        teste1[23] = 1;
+	}
+
+	// Conferindo posicoes
+	if(position(f)) printf("25-CORRETO\n");
+	else {
+	    printf("25-incorreto(posicoes nao atualizadas\n");
+        teste1[24] = 1;
+	}
+// 
+//-----------------------------------------------------------------------------------------------//
+// 
+	printf("\n\nTESTE 2 - INSERCAO\n\n");
+	f = criarFila(5);
+
+	int teste2[7];
+	for (int i = 0; i < 7; i++) {
+        teste2[i] = 0;
+    }
+	
+	// Inserir com prioridade em ponto flutuante
+	if (inserirElemento(f, 4, 1.337)) {
+		el = f -> arranjo[4];
+		if (el -> prioridade == 1.337f ) printf("1-CORRETO\n");
+		else {
+		    printf("1-incorreto(prioridade errada)\n");
+		    teste2[0] = 1;
+		}
+	} else {
+	    printf("1-incorreto(retornou false)\n");
+        teste2[0] = 1;
+	}
+	exibirLog(f);
+	 
+	// Conferindo os campos posicao
+	inserirElemento(f,2,24);
+	exibirLog(f);
+	 
+	inserirElemento(f,3,8.0085);
+	exibirLog(f);
+	 
+	if(position(f)) printf("2-CORRETO\n");
+	else {
+	    printf("2-incorreto(posicoes nao atualizadas\n");
+        teste2[1] = 1;
+	}
+	 
+	if(isMaxHeap(f)) printf("3-CORRETO\n");
+	else {
+	    printf("3-incorreto(propriedades do heap max quebradas)\n");
+        teste2[2] = 1;
+	}
+	 	
+	// Conferindo a insercao no arranjo
+	if (inserirElemento(f, 1, 42)) {
+		if (f -> arranjo[1]) printf("4-CORRETO\n");
+		else {
+		    printf("4-incorreto(pos no arranjo estah vazia)\n");
+            teste2[3] = 1;
+		}
+	} else {
+	    printf("4-incorreto(retornou false)\n");
+        teste2[3] = 1;
+	}
+	exibirLog(f);
+	  
+	// Conferindo a insercao no heap
+	if (inserirElemento(f, 0, 100)) {
+		if (f -> heap[0]) printf("5-CORRETO\n");
+		else {
+		    printf("5-incorreto(pos no heap estah vazia)\n");
+            teste2[4] = 1;
+		}
+	} else {
+	    printf("5-incorreto(retornou false)\n");
+        teste2[4] = 1;
+	}
+	 
+	// Conferindo se o ponteiro no heap eh o mesmo do ponteiro no arranjo
+	if (f -> heap[0] == f -> arranjo[0]) printf("6-CORRETO\n");
+	else {
+	    printf("6-incorreto(ponteiros no heap e arr de um elem nao sao iguais)\n");
+        teste2[5] = 1;
+	}
+	 
+	// Conferindo se o campo elementosNoHeap estah certo
+	if (f -> elementosNoHeap == 5) printf("7-CORRETO\n");
+	else {
+	    printf("7-incorreto(elementosNoHeap errado)\n");
+        teste2[6] = 1;
+	}
+
+// 
+//-----------------------------------------------------------------------------------------------//
+// 
+	printf("\n\nTESTE 3 - AUMENTAR E REDUZIR PRIORIDADE\n\n");
+	exibirLog(f);
+
+	int teste3[7];
+	for (int i = 0; i < 7; i++) {
+        teste3[i] = 0;
+		}
+		 
+	if (reduzirPrioridade(f, 0, 41)) printf("1-CORRETO\n");
+	else {
+	    printf("1-incorreto(retornou false\n");
+	    teste3[0] = 1;
+	}
+	exibirLog(f);
+	 
+	if(isMaxHeap(f)) printf("2-CORRETO\n");
+	else {
+	    printf("2-incorreto(propriedades do heap max quebradas)\n");
+        teste3[1] = 1;
+	}
+	 
+	if (aumentarPrioridade(f, 2, 44)) printf("3-CORRETO\n");
+	else {
+	    printf("3-incorreto(retornou false)\n");
+        teste3[2] = 1;
+	}
+	exibirLog(f);
+	 
+	if(isMaxHeap(f)) printf("4-CORRETO\n");
+	else {
+	    printf("4-incorreto(propriedades do heap max quebradas)\n");
+        teste3[3] = 1;
+	}
+	 
+	if (f -> arranjo[0] -> prioridade == 41) printf("5-CORRETO\n");
+	else {
+	    printf("5-incorreto(prioridade errada)\n");
+        teste3[4] = 1;
+	}
+	 
+	if (f -> arranjo[2] -> prioridade == 44) printf("6-CORRETO\n");
+	else {
+	    printf("6-incorreto(prioridade errada)\n");
+        teste3[5] = 1;
+	}
+	 
+	if(position(f)) printf("7-CORRETO\n");
+	else {
+	    printf("7-incorreto(posicoes nao atualizadas)");
+        teste3[6] = 1;
+	}
+
+	printf("\n--------------------------------------------------\n");
+
+	printf("TESTES 0:\n");
+	printf("RESULTADO TESTES 0: %i erros\n", countArray(teste0, 16));
+
+	printf("TESTES 1:\n");
+    printf("RESULTADO TESTES 1: %i erros\n", countArray(teste1, 25));
+
+	printf("TESTES 2:\n");
+    printf("RESULTADO TESTES 2: %i erros\n", countArray(teste2, 7));
+
+	printf("TESTES 3:\n");
+    printf("RESULTADO TESTES 3: %i erros\n", countArray(teste3, 7));
+
+
+}
